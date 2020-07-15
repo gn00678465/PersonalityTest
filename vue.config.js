78cef4ab9212572686ae779d6c97645de8b3ac65
MADAO <gn00678465@gmail.com>
2020-07-15 12:44:47 +0800

    fix title

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/PersonalityTest/'
-    : '/'
+    : '/',
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Personality traits test'
+        return args
+      })
+  }
 }
